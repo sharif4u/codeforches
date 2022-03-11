ac58247bb0e6ac06dab6c0752a1dceb018862e20
MD. SHARIF UDDIN <101212925+sharif4u@users.noreply.github.com>
2022-03-11 11:00:32 +0600

    Codeforches problem solution

---
+++ A.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    while (n--) {
+        string s;
+        cin >> s;
+
+        if (s.length() > 10) {
+            cout << s.front() << s.length() - 2 << s.back() << endl;
+        } else {
+            cout << s << endl;
+        }
+    }
+
+}
